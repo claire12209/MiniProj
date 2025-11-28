014e36e972ec5eee802b689dab385b2a95413187
claire12209 <112130516+claire12209@users.noreply.github.com>
2025-11-27 22:36:07 -0800

    Update miniProject.cpp

--- miniProject.cpp
+++ miniProject.cpp
@@ -10,31 +10,7 @@ int main() {
     populate_function_map();
 
     //select static typing
-    cout << "Dynamic(1) or Static(2)? ";
-
-
-    //get input string 
-    int myint = 0;
-    string s;
-    getline(cin, s, '\n');
-
-    myint = stoi(s);
-
-    //error checking
-    while (myint != 1 && myint != 2) {
-        cout << "Error, input a valid typing" << endl;
-        cout << "Dynamic(1) or Static(2)? " << endl;
-        getline(cin, s, '\n');
-        myint = stoi(s);
-
-    }
-    //set whether static or dynamic
-    if (myint == 1) {
-        staticvar = false;
-    }
-    else {
-        staticvar = true;
-    }
+  
 
     //start loop until quit is chosen
     while (end) {
